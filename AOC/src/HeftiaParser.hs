5ba0f9f75d3c37a1905c4e69f51cff2b6b84f078
alex800121 <alex800121@hotmail.com>
2024-12-06 10:59:50 +0800

    attempt msplit

--- AOC/src/HeftiaParser.hs
+++ AOC/src/HeftiaParser.hs
@@ -3,85 +3,94 @@
 module HeftiaParser where
 
 import Control.Applicative (Alternative (..), optional)
-import Control.Monad (unless)
+import Control.Arrow
+import Control.Monad (msum, unless)
 import Control.Monad.Hefty
 import Control.Monad.Hefty.NonDet hiding (empty)
 import Control.Monad.Hefty.State
+import Data.Char (isNumber)
+import Data.Effect.NonDet qualified as NonDet
 import Data.List (stripPrefix)
 import Data.Maybe (fromMaybe)
 import Data.Proxy (Proxy)
-import Data.Char (isNumber)
-import Control.Arrow
 
 runNonDetMaybe :: forall ef a. Eff '[] (Choose ': Empty ': ef) a -> Eff '[] ef (Maybe a)
 runNonDetMaybe =
   bundleN @2
     >>> interpretBy
       (pure . pure)
-      ( ( \Choose k -> do
-            x <- k False
-            case x of
-              Just y -> pure (pure y)
-              Nothing -> k True
-        )
+      ( (\Choose k -> maybe (k True) (pure . pure) =<< k False)
           !+ (\Empty _ -> pure Nothing)
           !+ nil
       )
 
-try :: forall a s eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => Eff eh ef a -> Eff eh ef a
-try m = do
-  s <- get @String
-  m <|> (put s >> empty)
+-- msplit :: forall ef eh a. (Choose <| ef, ChooseH <<| eh, Empty <| ef) => Eff eh ef a -> Eff eh ef (Maybe (a, Eff eh ef a))
+-- msplit = loop []
+--   where
+--     loop jq (Val a) = pure (Just (a, msum jq))
+--     loop jq (Op u q) = case u of
+--       Left uh -> case prjH @ChooseH uh of
+--         Nothing -> Op u k
+--         Just (ChooseH a b) -> undefined
+--       Right uf -> case prj @Empty uf of
+--         Nothing -> undefined
+--         Just Empty -> undefined
+--       where
+--         k = _f q (loop jq)
 
-satisfy :: forall a eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => (Char -> Bool) -> Eff eh ef Char
+-- "try" may not be needed, because proper backtracking is default
+-- try :: forall a s eh ef. (Empty <| ef, State String <| ef) => Eff eh ef a -> Eff eh ef a
+-- try = id
+
+satisfy :: forall a eh ef. (Empty <| ef, State String <| ef) => (Char -> Bool) -> Eff eh ef Char
 satisfy p = do
   s <- get
   case s of
     c : r | p c -> put r >> pure c
-    _ -> empty
+    _ -> NonDet.empty
 
-anySingle :: (ChooseH <<| eh, Empty <| ef, State String <| ef) => Eff eh ef Char
+anySingle :: (Empty <| ef, State String <| ef) => Eff eh ef Char
 anySingle = satisfy (const True)
 
-char :: forall a eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => Char -> Eff eh ef Char
+char :: forall a eh ef. (Empty <| ef, State String <| ef) => Char -> Eff eh ef Char
 char c = satisfy (== c)
 
-string :: forall eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => String -> Eff eh ef String
+string :: forall eh ef. (Empty <| ef, State String <| ef) => String -> Eff eh ef String
 string s = do
   i <- get
   case stripPrefix s i of
-    Nothing -> empty
+    Nothing -> NonDet.empty
     Just i' -> put i' >> pure s
 
-takeWhileP :: forall a eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => (Char -> Bool) -> Eff eh ef String
+takeWhileP :: forall a eh ef. (Empty <| ef, State String <| ef) => (Char -> Bool) -> Eff eh ef String
 takeWhileP p = do
   (a, s) <- span p <$> get
   put s
   pure a
 
-takeWhile1P :: forall a eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => (Char -> Bool) -> Eff eh ef String
+takeWhile1P :: forall a eh ef. (Empty <| ef, State String <| ef) => (Char -> Bool) -> Eff eh ef String
 takeWhile1P p = (:) <$> satisfy p <*> takeWhileP p
 
-takeRest :: forall a eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => Eff eh ef String
+takeRest :: forall a eh ef. (Empty <| ef, State String <| ef) => Eff eh ef String
 takeRest = do
   s <- get @String
   put @String []
   pure s
 
-takeP :: forall a eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => Int -> Eff eh ef String
+takeP :: forall a eh ef. (Empty <| ef, State String <| ef) => Int -> Eff eh ef String
 takeP n = do
   s <- get
-  if length s >= n then let (a, b) = splitAt n s in put b >> pure a else empty
+  if length s >= n then let (a, b) = splitAt n s in put b >> pure a else NonDet.empty
 
-signedInteger :: forall a eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => Eff eh ef Int
-signedInteger = try @Int @String do
-  sign <- (char '-' >> pure negate) <|> (optional (char '+') >> pure id)
+signedInteger :: forall a eh ef. (Choose <| ef, Empty <| ef, State String <| ef) => Eff eh ef Int
+signedInteger = do
+  sign <- (char '-' >> pure negate) `branch` ((char '+' `branch` pure '+') >> pure id)
   n <- read @Int <$> takeWhile1P isNumber
   pure $ sign n
 
-eof :: forall a eh ef. (ChooseH <<| eh, Empty <| ef, State String <| ef) => Eff eh ef ()
+eof :: forall a eh ef. (Empty <| ef, State String <| ef) => Eff eh ef ()
 eof = do
   s <- get @String
-  unless (null s) empty
+  unless (null s) NonDet.empty
 
-parseMaybe p input = runPure $ evalState input $ runNonDet $ runChooseH (p <* eof)
+parseMaybe p input = runPure $ evalState input $ runNonDet (p <* eof)
